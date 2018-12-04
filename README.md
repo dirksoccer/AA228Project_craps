050cbbb91a053dbf549b51c1a353f852425f9e92
dirksoccer <>
2018-12-03 23:24:53 -0800

    Winnings properly distributed in function rollTheDice

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AA228Project_craps
 Decision making algorithm for the casino game craps
+
+Rewards calculated in function "rollTheDice" assume the cost of placing the bet was applied when placing the bet (ie - the cost was the amount placed, the reward is the amount bet * (odds paid + 1))
--- craps_core.py
+++ craps_core.py
@@ -6,8 +6,98 @@ def rollTheDice(wager,odds,point = 0):
 	#Initialize flag to signal if round is over
 	roundOver = 0
 
+	#Initialize results of the round
+	result = 0
+
 	#Roll 2d6 and return total
-	roll = rnd(1,7)+rnd(1,7)
+	die1 = rnd(1,7)
+	die2 = rnd(1,7)
+	roll = die1+die2
+
+	#Check for single roll bets
+	if roll == 2:
+		result += wager['snakeeyes']*(odds['snakeeyes']+1)
+		result += wager['hiLo']*(odds['hiLo']+1)
+		result += wager['anyCraps']*(odds['anyCraps']+1)
+		result += wager['cAndE']*(odds['cAndE'][roll]+1)
+		result += wager['field']*(odds['field'][roll]+1)
+		result += wager['horn']*(odds['horn'][roll]+1)
+		result += wager['world']*(odds['world'][roll]+1)
+
+	elif roll == 3:
+		result += wager['aceDuece']*(odds['aceDuece']+1)
+		result += wager['anyCraps']*(odds['anyCraps']+1)
+		result += wager['cAndE']*(odds['cAndE'][roll]+1)
+		result += wager['field']*(odds['field'][roll]+1)
+		result += wager['horn']*(odds['horn'][roll]+1)
+		result += wager['world']*(odds['world'][roll]+1)
+
+	elif roll == 4:
+		result += wager['field']*(odds['field'][roll]+1)
+		result += wager['place4']*(odds['place4']+1)
+		result += wager['buy4']*(odds['buy4']+0.95)
+		result += wager['lay4']*(odds['lay4']*0.95+1)
+
+		if die1 == die2:
+			result += wager['hard4']*(odds['hard4']+1)
+
+	elif roll == 5:
+		result += wager['place5']*(odds['place5']+1)
+		result += wager['buy5']*(odds['buy5']+0.95)
+		result += wager['lay5']*(odds['lay5']*0.95+1)
+
+	elif roll == 6:
+		result += wager['big6']*(odds['big6']+1)
+		result += wager['place6']*(odds['place6']+1)
+		result += wager['buy6']*(odds['buy6']+0.95)
+		result += wager['lay6']*(odds['lay6']*0.95+1)
+
+		if die1 == die2:
+			result += wager['hard6']*(odds['hard6']+1)
+
+	elif roll == 7:
+		result += wager['bigRed']*(odds['bigRed']+1)
+		result += wager['world']*(odds['world'][roll]+1)
+
+	elif roll == 8:
+		result += wager['big8']*(odds['big8']+1)
+		result += wager['place8']*(odds['place8']+1)
+		result += wager['buy8']*(odds['buy8']+0.95)
+		result += wager['lay8']*(odds['lay8']*0.95+1)
+
+		if die1 == die2:
+			result += wager['hard8']*(odds['hard8']+1)
+
+	elif roll == 9:
+		result += wager['field']*(odds['field'][roll]+1)
+		result += wager['place9']*(odds['place9']+1)
+		result += wager['buy9']*(odds['buy9']+0.95)
+		result += wager['lay9']*(odds['lay9']*0.95+1)
+
+	elif roll == 10:
+		result += wager['field']*(odds['field'][roll]+1)
+		result += wager['place10']*(odds['place10']+1)
+		result += wager['buy10']*(odds['buy10']+0.95)
+		result += wager['lay10']*(odds['lay10']*0.95+1)
+
+		if die1 == die2:
+			result += wager['hard10']*(odds['hard10']+1)
+
+	elif roll == 11:
+		result += wager['yoleven']*(odds['yoleven']+1)
+		result += wager['cAndE']*(odds['cAndE'][roll]+1)
+		result += wager['field']*(odds['field'][roll]+1)
+		result += wager['horn']*(odds['horn'][roll]+1)
+		result += wager['world']*(odds['world'][roll]+1)
+
+	elif roll == 12:
+		result += wager['boxcars']*(odds['boxcars']+1)
+		result += wager['hiLo']*(odds['hiLo']+1)
+		result += wager['cAndE']*(odds['cAndE'][roll]+1)
+		result += wager['field']*(odds['field'][roll]+1)
+		result += wager['horn']*(odds['horn'][roll]+1)
+		result += wager['world']*(odds['world'][roll]+1)
+
 
 	#Handle "Come Out" roll
 	if point == 0:
@@ -15,20 +105,20 @@ def rollTheDice(wager,odds,point = 0):
 		#Check for "Natural"
 		if roll in [7,11]:
 
-			result = wager['pass']-wager['dontPass'] #Calculate winnings
+			result += wager['pass']*(odds['pass']+1) #Calculate winnings
 			roundOver = 1							 #Flag round is over
 			return(roll,result,roundOver)
 
 		#Check for "Craps"
 		elif roll in [2,3,12]:
 
-			result = wager['dontPass']-wager['pass']
+			result += wager['dontPass'](odds['dontPass']+1)
 			roundOver = 1
 			return(roll,result,roundOver)
 
 		#Establish Point
 		else:
-			result = 0
+
 			return(roll,result,roundOver)
 
 	#Handle "Point" rolls
@@ -37,15 +127,24 @@ def rollTheDice(wager,odds,point = 0):
 		#Check for "Seven-Out"
 		if roll == 7:
 
-			result = wager['dontPass']-wager['pass']+wager['dontCome']-wager['come']
+			result += wager['dontPass'](odds['dontPass']+1)+wager['dontCome'](odds['dontCome']+1)
+			result += wager['dontPassOdds']*(odds['dontPassOdds'][point]+1)
+			result += wager['dontComeOdds']*(odds['dontComeOdds'][point]+1)
 			roundOver = 1
 			return(roll,result,roundOver)
 
 		#Check for "Hit"
 		elif roll == point:
 
-			result = wager['pass']-wager['dontPass']
+			result += wager['pass'](odds['pass']+1)+wager['come'](odds['come']+1)
+			result += wager['passOdds']*(odds['passOdds']+1)
+			result += wager['comeOdds']*(odds['comeOdds']+1)
 			roundOver = 1
+			return(roll,result,roundOver)
+
+		#Continue round
+		else:
+
 			return(roll,result,roundOver)
 
 
@@ -126,8 +225,8 @@ def main():
 			'lay8':(5.0/6),
 			'lay9':(2.0/3),
 			'lay10':.5,
-			'putPass':0,
-			'putCome':0,
+			'putPass':1,
+			'putCome':1,
 			'hard4':7,
 			'hard6':9,
 			'hard8':9,
@@ -146,4 +245,8 @@ def main():
 			'world':{2:(26.0/5),3:(11.0/5),7:0,11:(11.0/5),12:(26.0/5)},
 			'field':{2:2,3:1,4:1,9:1,10:1,11:1,12:2}}
 
-	rollTheDice(wager,odds)
+	print('test')
+	print(rollTheDice(wager,odds))
+
+if __name__ == '__main__':
+    main()
